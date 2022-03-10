e9ff30f2687ca7e3317869378bc1cab49caf9805
oliverbskau <oliverbskau@gmail.com>
2022-03-10 19:10:16 +0100

    done

---
+++ app.js
@@ -0,0 +1,13 @@
+const express = require("express");
+const app = express();
+
+app.use(express.json());
+
+app.get("/", (req, res) => {
+  res.sendFile(__dirname + "/src/App.svelte");
+});
+
+const PORT = 8080;
+app.listen(process.env.PORT || "5000", (error) => {
+  console.log("Server is running on port", PORT);
+});
